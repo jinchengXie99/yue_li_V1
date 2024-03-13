3fc55a549784710eaee68dd2d0cd3b4768926417
jinchengXie99 <chengdiy@foxmail.com>
2024-03-13 17:41:32 +0800

    更改温控计算滤波 改善跳变

--- 月立220/Obj/pid_regulators.d
+++ 月立220/Obj/pid_regulators.d
@@ -1,5 +1,5 @@
 .\obj\pid_regulators.o: Kernal_Source\PID_regulators.c
-.\obj\pid_regulators.o: C:\Users\jincheng.xie\AppData\Local\Arm\Packs\Linko\LKS03x\1.1.5\Device\Include\basic.h
+.\obj\pid_regulators.o: C:\Users\yun.chen\AppData\Local\Arm\Packs\Linko\LKS03x\1.1.5\Device\Include\basic.h
 .\obj\pid_regulators.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
 .\obj\pid_regulators.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\ctype.h
 .\obj\pid_regulators.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
